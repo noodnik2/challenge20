752c8f5dd9e6fd8c3a70cb87eee6ec33a5669ce2
Marty Ross <>
2020-06-11 16:48:49 -0700

    Checkpoint save

---
+++ hr/java8/src/test/java/noodnik/autodesk/FibonacciTests.java
@@ -0,0 +1,43 @@
+package noodnik.autodesk;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import static noodnik.lib.Common.*;
+
+public class FibonacciTests {
+
+    int fibonacci(int i) {
+        if (i < 2) {
+            return i;
+        }
+        return fibonacci(i - 1) + fibonacci(i - 2);
+    }
+    
+    @Test
+    public void testCase0() {
+        assertEquals(0, fibonacci(0));
+    }
+    
+    @Test
+    public void testCase1() {
+        assertEquals(1, fibonacci(1));
+    }
+    
+    @Test
+    public void testCase2() {
+        assertEquals(1, fibonacci(2));
+    }
+    
+    @Test
+    public void testCase3() {
+        assertEquals(2, fibonacci(3));
+    }
+    
+    @Test
+    public void testCase4() {
+        assertEquals(3, fibonacci(4));
+    }
+    
+    
+}
